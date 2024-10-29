90bbb992bbc7ee316bac39ebf76d637f56aea018
adamelalfy <143737572+adamelalfy@users.noreply.github.com>
2024-10-29 09:58:43 -0400

    prog done

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
 
-    [SerializeField] private GameObject _citrouille;
-
     private int _score = 0;
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Citrouille"))
         {
             citrouilleDropped();
+            Destroy(other.gameObject);
         }
     }
     private void citrouilleDropped() {
         _score ++;
+        Debug.Log("La variable int du score est à une valeur de : " + _score);
+
     }
 
 }
